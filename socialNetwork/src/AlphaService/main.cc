ca12807cdc92ec16109dd8fe4c4dc4ea2fc2deda
Barber0 <simonfangzilin@outlook.com>
2021-08-03 19:42:55 +0000

    add: test service AlphaService

---
+++ socialNetwork/src/AlphaService/main.cc
@@ -0,0 +1,19 @@
+#ifndef ALPHA
+#define ALPHA
+
+#include <iostream>
+#include <thrift/transport/TServerSocket.h>
+
+using ::apache::thrift::transport::TServerSocket;
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "alpha" << std::endl;
+    auto serverSocket = std::make_shared<TServerSocket>(
+        "0.0.0.0",
+        8080
+    );
+    return 0;
+}
+
+#endif
