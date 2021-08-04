40d63a0ddb2924b91d4bc71eee037f3a199798ae
Barber0 <simonfangzilin@outlook.com>
2021-08-04 12:53:46 +0800

    update: modify cluster config to adapt to k8s

--- socialNetwork/openshift/scripts/create-all-configmap.sh
+++ socialNetwork/openshift/scripts/create-all-configmap.sh
@@ -2,6 +2,8 @@
 
 cd $(dirname $0)/..
 
+source scripts/controll.sh
+
 ./scripts/configmaps/create-jaeger-configmap.sh
 ./scripts/configmaps/create-media-frontend-configmap.sh
 ./scripts/configmaps/create-nginx-thrift-configmap.sh
